8eee3e0bd851a217fc81025f9766ccd4fc36ff87
Earbaj <earbaj@evidentbd.com>
2024-11-22 12:08:18 +0600

    design welcome page

--- lib/ui/splash_screen.dart
+++ lib/ui/splash_screen.dart
@@ -5,6 +5,115 @@ class SplashScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: const Color(0xFF111B21),
+      body: Center(
+        child: Column(
+          children: [
+            Expanded(
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 50,
+                    vertical: 10,
+                  ),
+                  child: Image.asset(
+                    'assets/images/circle.png',
+                    color: const Color(0xFF00A884),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 40),
+            Expanded(
+                child: Column(
+              children: [
+                const Text(
+                  'Welcome to WhatsApp',
+                  style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white),
+                ),
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+                  child: RichText(
+                    textAlign: TextAlign.center,
+                    text: const TextSpan(
+                      text: 'Read our ',
+                      style: TextStyle(height: 1.5, color: Color(0xFF8696A0)),
+                      children: [
+                        TextSpan(
+                          text: 'Privacy Policy. ',
+                          style: TextStyle(color: Color(0xFF53BDEB)),
+                        ),
+                        TextSpan(
+                            text: 'Tap "Agree and continue" to accept the '),
+                        TextSpan(
+                          text: 'Terms of Services.',
+                          style: TextStyle(color: Color(0xFF53BDEB)),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 42,
+                  width: MediaQuery.of(context).size.width - 100,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xFF00A884),
+                        foregroundColor: const Color(0xFF111B21),
+                        splashFactory: NoSplash.splashFactory,
+                        elevation: 0,
+                        shadowColor: Colors.transparent),
+                    child: const Text("AGREE AND CONTINUE"),
+                  ),
+                ),
+                const SizedBox(height: 50),
+                Material(
+                  color: const Color(0xFF182229),
+                  borderRadius: BorderRadius.circular(20),
+                  child: InkWell(
+                    onTap: () {},
+                    borderRadius: BorderRadius.circular(20),
+                    splashFactory: NoSplash.splashFactory,
+                    highlightColor: const Color(0xFF09141A),
+                    child: const Padding(
+                      padding:  EdgeInsets.symmetric(
+                        horizontal: 16,
+                        vertical: 8.0,
+                      ),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Icon(
+                            Icons.language,
+                            color: Color(0xFF00A884),
+                          ),
+                          SizedBox(width: 10),
+                          Text(
+                            'English',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          SizedBox(width: 10),
+                          Icon(
+                            Icons.keyboard_arrow_down,
+                            color: Color(0xFF00A884),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            )),
+          ],
+        ),
+      ),
+    );
   }
 }
